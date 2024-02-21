122dbe6652819ae59543af3fcccb287e24f0823f
ParkerHarrelson <parkerharrelson13@gmail.com>
2024-02-21 12:43:04 -0600

    enabling window creation with a specific size and some resize functionality

--- src/Solar System Simulator.cpp
+++ src/Solar System Simulator.cpp
@@ -5,7 +5,21 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+static void error_callback(int error, const char* description) {
+    std::cerr << "Error: " << description << std::endl;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
 int main(void) {
+    glfwSetErrorCallback(error_callback);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); 
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
     GLFWwindow* window;
 
     if (!glfwInit()) {
@@ -13,20 +27,42 @@ int main(void) {
         return -1;
     }
 
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window) {
-        glfwTerminate();
-        std::cerr << "Failed to open GLFW window\n";
-        return -1;
-    }
+    const GLFWvidmode* mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
 
-    glfwMakeContextCurrent(window);
-    glewInit();
+    if (mode != nullptr) {
 
-    while (!glfwWindowShouldClose(window)) {
-        glClear(GL_COLOR_BUFFER_BIT);
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+        int screenWidth = mode->width;
+        int screenHeight = mode->height;
+
+        printf("Screen resolution: %d x %d\n", screenWidth, screenHeight);
+        printf("Scaling down resolution from full screen...");
+        
+        int windowWidth = static_cast<int>(screenWidth * 0.75);
+        int windowHeight = static_cast<int>(screenHeight * 0.75);
+
+        window = glfwCreateWindow(windowWidth, windowHeight, "Solar System Simulator", NULL, NULL);
+        if (!window) {
+            glfwTerminate();
+            std::cerr << "Failed to open GLFW window\n";
+            return -1;
+        }
+
+        glfwSetWindowPos(window, (screenWidth - windowWidth) / 2, (screenHeight - windowHeight) / 2);
+
+        glfwMakeContextCurrent(window);
+
+        glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+        if (glewInit() != GLEW_OK) {
+            std::cerr << "Failed to initialize window...\n";
+            return -1;
+        }
+
+        while (!glfwWindowShouldClose(window)) {
+            glClear(GL_COLOR_BUFFER_BIT);
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
     }
 
     glfwTerminate();
