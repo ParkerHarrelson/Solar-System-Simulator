2336486c3761ad2a1c22f30891155afee15bf71f
ParkerHarrelson <parkerharrelson13@gmail.com>
2024-02-22 01:51:14 -0600

    I forgot what I changed

--- src/utils/MathUtils.cpp
+++ src/utils/MathUtils.cpp
@@ -11,3 +11,16 @@ double MathUtils::calculateGravitationalForceBetweenMasses(const SolarSystem::Ce
 
     return GRAVITATIONAL_CONSTANT * ((bodyOne.getMass() * bodyTwo.getMass()) / (euclideanDistance * euclideanDistance));
 }
+
+// 5,890,329,911 == 100 // Mercury and Pluto
+// 37,236,121,041,383 == 90 // earth and titan
+// 49,960,826,401,321,459 == 50// pluto and sun
+// 35,426,928,201,758,164,374,160 == 10 // earth and sun
+
+// map where the key is the pair of the two objects (earth and sun for example)
+// value for the key is a Pair of Integer and Double
+// The integer is the Score and Double is the last calculated Force
+// each frame we will iterate the map and get each key and get the Pair Value from it.
+// if the integer value == 0 then recalculate the force for that pair and then based on that force calculate
+// the new position and then set a new value for that key based on that force
+// if the integer value != 0 then decrement the value and then grab the already existing force and use some sort linear interpolation to approximate correct position
